4dd551ca114bc0b3f2919f58d2538e45bb8b6382
Vladimir Krylov <vladimir.krylov@perfectline.co>
2015-06-08 20:54:32 +0300

    add posibility to upload files into subfolders

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,6 +1,6 @@
 class Folder < ActiveRecord::Base
   acts_as_nested_set
-  has_many :files, class_name: UserFile
+  has_many :files, class_name: "UserFile"
   validates :name, presence: true, allow_blank: true
   validates :name, uniqueness: true
 
